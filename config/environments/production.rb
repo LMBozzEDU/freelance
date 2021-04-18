fc12e75708220afdeefc19902cbdc049a340f234
jaminio <59372236+jaminio@users.noreply.github.com>
2021-04-18 03:34:02 +0100

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
- config.action_mailer.default_url_options = { host: 'freelance-ruky-a.herokuapp.com'}
+ config.action_mailer.default_url_options = { host: 'testzc.herokuapp.com/'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
