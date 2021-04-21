a0074efa6fd61d2a54bae1b28a8d7722cf1a6ae4
joshfromfirehosting <82917498+joshfromfirehosting@users.noreply.github.com>
2021-04-21 02:49:44 +0100

    Update test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,6 +6,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # added
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # added end 
+  
   config.cache_classes = false
 
   # Do not eager load code on boot. This avoids loading your whole application
